95a073cbbc460d526de724b33ee29421f3fe7210
Tanish Kumar <tanish.kumar.works@gmail.com>
2026-06-22 11:23:11 +0530

    feat: add vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Analytics } from '@vercel/analytics/react';
 
 import './index.css';
 import App from './App.jsx';
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </Provider>
   </StrictMode>
